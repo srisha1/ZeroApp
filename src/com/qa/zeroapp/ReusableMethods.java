c4c3f3ed47a05c9ce57651a4bc5a5c35a0093b2f
srisha1 <edrsiri@gmail.com>
2020-03-21 17:57:01 -0700

    test1

--- src/com/qa/zeroapp/ReusableMethods.java
+++ src/com/qa/zeroapp/ReusableMethods.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import com.relevantcodes.extentreports.ExtentReports;
--- src/com/qa/zeroapp/ZeroAppTestCases.java
+++ src/com/qa/zeroapp/ZeroAppTestCases.java
@@ -4,18 +4,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.relevantcodes.extentreports.LogStatus;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ZeroAppTestCases extends ReusableMethods {
 
